cff5d31d940967e3f24871f1770fb7c790cd29c9
Caleb <hcgtzol@gmail.com>
2023-09-10 15:38:58 -0500

    Actualizado para que se muestren ciertas cosas

--- grafica.py
+++ grafica.py
@@ -5,6 +5,14 @@ ologn=log(on)
 on2=on**2
 onlogn=on*log(on)
 o2n=2**on
-plot(on, on, 'b.-', on, ologn, 'g.-', on, on2, 'r.-', on, onlogn, 'c.-', on, o2n, 'm.-')
+o1=ones_like(on)
+plot(on, on, 'b.-', label ='O(n)')
+plot(on, ologn, 'g.-', label ='O(log n)')
+plot(on, on2, 'r.-', label ='O(n2)')
+plot(on, onlogn, 'c.-', label ='O(n log n )')
+plot(on, o2n, 'm.-', label ='O(2n)')
+plot(on, o1, 'y.-', label ='O(1)')
 
+
+legend()
 show()
